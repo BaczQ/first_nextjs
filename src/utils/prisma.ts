3520b94b0b73d2f99d66839152fd0f1bcea67a8f
Webakov <webakov@yandex.ru>
2025-12-08 12:36:31 +0700

    1

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,8 +1,29 @@
-// lib/prisma.ts
-import { PrismaClient } from "../../generated/prisma";
+// src/utils/prisma.ts
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pkg from "pg";
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const { Pool } = pkg;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// создаём пул соединений
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// создаём адаптер
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = global as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
